52691adae55ea20a2ea0ed4429781551c8fe8de2
Oleg Shoshin <>
2023-01-04 00:40:29 +0300

    1. Initial project setup

---
+++ buildSrc/buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,16 @@
+object Dependencies {
+
+    object Kotlin {
+        private const val version = "1.7.10"
+        const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
+    }
+
+    object Compose {
+        private const val version = "1.2.0"
+        const val gradlePlugin = "org.jetbrains.compose:compose-gradle-plugin:$version"
+    }
+
+    object Android {
+        const val gradlePlugin = "com.android.tools.build:gradle:7.2.2"
+    }
+}
